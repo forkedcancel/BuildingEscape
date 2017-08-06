abc3385c81bdeb0ebb723dc69f389aa1adcdb822
William Rose <will@williamro.se>
2017-08-06 12:37:42 -0400

    Door opens with spotlit trigger area, closes just before you can get to it

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -1,37 +1,45 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "GameFramework/Actor.h"
 #include "OpenDoor.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 
 // Sets default values for this component's properties
-UOpenDoor::UOpenDoor()
-{
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+UOpenDoor::UOpenDoor() {
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 }
 
 
 // Called when the game starts
-void UOpenDoor::BeginPlay()
-{
-	Super::BeginPlay();
+void UOpenDoor::BeginPlay() {
+    Super::BeginPlay();
 
-	// ...
-	AActor *owner = GetOwner();
-    FRotator rotator = FRotator(0.f, -60.f, 0.f);
-	owner->SetActorRotation(rotator);
-	
+    ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+    owner = GetOwner();
+}
+
+void UOpenDoor::OpenDoor() const {
+    owner->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
 }
 
 
 // Called every frame
-void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+    if (PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+        LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+        OpenDoor();
+    }
+
+    if (GetWorld()->GetTimeSeconds() > LastDoorOpenTime + DoorCloseDelay) {
+        CloseDoor();
+    }
+}
+
+void UOpenDoor::CloseDoor() const {
+    owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
 }
 
--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -8,27 +8,41 @@
 #include "OpenDoor.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
-{
-	GENERATED_BODY()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 
-public:	
-	// Sets default values for this component's properties
-	UOpenDoor();
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent {
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this component's properties
+    UOpenDoor();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+    // Called when the game starts
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:
+    // Called every frame
+    virtual void
+    TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 
 private:
-    UPROPERTY(VisibleAnywhere)
-    float OpenAngle = 90.f;
-		
-	UPROPERTY(EditAnywhere)
-    ATriggerVolume* PressurePlate;
+    UPROPERTY(EditAnywhere)
+    float OpenAngle = -90.f;
+
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume *PressurePlate;
+
+    UPROPERTY(EditAnywhere)
+    float DoorCloseDelay = 0.9f;
+
+    float LastDoorOpenTime;
+
+    AActor *ActorThatOpens;
+
+    AActor *owner;
+
+    void OpenDoor() const;
+
+    void CloseDoor() const;
 };
